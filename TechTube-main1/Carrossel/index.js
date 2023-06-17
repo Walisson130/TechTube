e67e0bf8581827c8a8dc813411594e983206f839
Walisson130 <105826730+Walisson130@users.noreply.github.com>
2023-06-17 15:55:58 -0300

    Add files via upload
    nova versao

---
+++ TechTube-main1/Carrossel/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const carousel = document.querySelector('.carousel');
+    const slides = carousel.querySelector('.slides');
+    const slideWidth = carousel.offsetWidth;
+    const buttons = carousel.querySelectorAll('.navigation button');
+  
+    let currentIndex = 0;
+  
+    buttons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        buttons[currentIndex].classList.remove('active');
+        currentIndex = index;
+        buttons[currentIndex].classList.add('active');
+        slides.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
+      });
+    });
+  });
